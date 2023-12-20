f6dee439a84e0f9297253e1cde4cf5f9f62fde1d
Thomas <thomastb04@gmail.com>
2023-12-20 13:25:29 +0100

    Added empty file to house makro-function

---
+++ R/parallel_news_crawler.R
@@ -0,0 +1,7 @@
+# retrieve news items by crawling selected topics on both or either of the two websites
+
+# retrieve contents from the news items by scraping the HTML structure of the web pages
+
+# save the contents in a data frame
+
+
